e3692626d1e18cc535af856f7f64c8cb382d57aa
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2024-10-01 19:21:38 +0530

    Object Creation

---
+++ ObjectCreation/src/ObjectCreation.java
@@ -0,0 +1,7 @@
+public class ObjectCreation {
+    public static void main(String[] args) {
+//        Object creation using new keyword
+        ObjectCreation objectName = new ObjectCreation();
+        System.out.println(objectName);
+    }
+}
